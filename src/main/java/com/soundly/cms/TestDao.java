3d27c9b39b3212cff0956848b2b6252b18acccd7
tuorao <tuorao@naver.com>
2015-09-07 11:52:24 +0900

    maven update

--- src/main/java/com/soundly/cms/TestDao.java
+++ src/main/java/com/soundly/cms/TestDao.java
@@ -1,15 +1,20 @@
 package com.soundly.cms;
 
+import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by Administrator on 2015-09-07.
  */
 @Repository
-public class TestDao extends SqlSessionDaoSupport {
+public class TestDao {
+
+    @Autowired
+    SqlSession sqlSession;
 
     public TestDTO sel(){
-        return getSqlSession().selectOne("test.sel");
+        return sqlSession.selectOne("test.sel");
     }
 }
